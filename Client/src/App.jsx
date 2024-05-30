fc539a306232bdb6ba683f7b99fa0c8f2d93567f
sajani <it20117696@my.sliit.lk>
2024-05-30 20:21:41 +0530

    user login and signup pages

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,8 +1,16 @@
-import { useState } from "react";
-import Login from "./pages/Login";
+import { Routes, Route } from "react-router-dom";
+import Login from "./components/User/Login";
+import SignUp from "./components/User/SignUp";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
-  return <Login />;
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
+  );
 }
 
 export default App;
